caa6dabb9bb9c472c38163219682f4d5e08b96f7
yeonju <97602748+yeondooo@users.noreply.github.com>
2023-07-30 23:59:33 +0900

    feat: 회원가입 페이지 ui 구현

---
+++ lib/presentation/sign_up/sign_up_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SignUpScreen extends StatelessWidget {
+  const SignUpScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('회원가입 페이지'),
+      ),
+      body: Container(),
+    );
+  }
+}
