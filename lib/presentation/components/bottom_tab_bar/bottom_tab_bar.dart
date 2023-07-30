162822ef90a07372623582558a5d896501516d71
yeonju <97602748+yeondooo@users.noreply.github.com>
2023-07-30 23:57:55 +0900

    feat: bottom navigation bar 구현

---
+++ lib/presentation/components/bottom_tab_bar/bottom_tab_bar.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:women_tech_flutter/presentation/main/main_screen.dart';
+import 'package:women_tech_flutter/presentation/sign_in/sign_in_screen.dart';
+
+class BottomTabBar extends StatefulWidget {
+  const BottomTabBar({Key? key}) : super(key: key);
+
+  @override
+  State<BottomTabBar> createState() => _BottomTabBarState();
+}
+
+class _BottomTabBarState extends State<BottomTabBar> {
+  int _currentIndex = 0;
+
+  final _pages = const [
+    MainScreen(),
+    SignInScreen(),
+    MainScreen(),
+    MainScreen(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _pages[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        currentIndex: _currentIndex,
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: '홈',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.flag),
+            label: '실천',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.school),
+            label: '교육',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: '나',
+          ),
+        ],
+      ),
+    );
+  }
+}
