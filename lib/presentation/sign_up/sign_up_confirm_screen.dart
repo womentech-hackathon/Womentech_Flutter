3ff2bd0c9a7abc24792703768bc15722d704da5c
yeonju <97602748+yeondooo@users.noreply.github.com>
2023-08-02 17:42:21 +0900

    feat: 회원가입 완료창 ui 구현

---
+++ lib/presentation/sign_up/sign_up_confirm_screen.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+
+class SignUpConfirmScreen extends StatelessWidget {
+  const SignUpConfirmScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(''),
+      ),
+      body: Column(
+        children: [
+          const Expanded(
+            child: Padding(
+              padding: EdgeInsets.all(20.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    '회원가입이 성공적으로 \n완료되었습니다!',
+                    style: TextStyle(
+                      fontSize: 26,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                  SizedBox(height: 40),
+                  Text(
+                    '이제 이루다가 재취업을 도와드릴 수 \n있도록, 원하는 직업과 실천사항을 \n작성하러 가보실까요?',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            style: ButtonStyle(
+              backgroundColor: MaterialStateProperty.all<Color>(
+                const Color(0xff5C82FC),
+              ),
+              minimumSize: MaterialStateProperty.all(
+                const Size(double.infinity, 98),
+              ),
+              shape: MaterialStateProperty.all<BeveledRectangleBorder>(
+                BeveledRectangleBorder(
+                  borderRadius: BorderRadius.circular(0),
+                ),
+              ),
+            ),
+            child: const Text(
+              '다음',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 20,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
