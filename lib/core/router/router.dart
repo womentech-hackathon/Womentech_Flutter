57bc1b8f32963aaae3e9ec36bce25117ec51eb29
yeonju <97602748+yeondooo@users.noreply.github.com>
2023-08-02 17:42:52 +0900

    fix: 회원가입 확인 화면 router 연결

--- lib/core/router/router.dart
+++ lib/core/router/router.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:women_tech_flutter/presentation/components/bottom_tab_bar/bottom_tab_bar.dart';
 import 'package:women_tech_flutter/presentation/sign_in/sign_in_screen.dart';
+import 'package:women_tech_flutter/presentation/sign_up/sign_up_confirm_screen.dart';
 import 'package:women_tech_flutter/presentation/sign_up/sign_up_screen.dart';
 
 final GoRouter router = GoRouter(
@@ -22,6 +23,12 @@ final GoRouter router = GoRouter(
           path: 'signup',
           builder: (BuildContext context, GoRouterState state) {
             return const SignUpScreen();
+          },
+        ),
+        GoRoute(
+          path: 'signup-confirm',
+          builder: (BuildContext context, GoRouterState state) {
+            return const SignUpConfirmScreen();
           },
         ),
       ],
