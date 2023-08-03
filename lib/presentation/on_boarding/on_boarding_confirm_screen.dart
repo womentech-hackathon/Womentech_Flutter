c2bec766f59a5d436a5978e6a319902e22097757
yeonju <97602748+yeondooo@users.noreply.github.com>
2023-08-03 23:01:07 +0900

    feat: 온보딩 완료 페이지 구현

---
+++ lib/presentation/on_boarding/on_boarding_confirm_screen.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+class OnBoardingConfirmScreen extends StatefulWidget {
+  const OnBoardingConfirmScreen({Key? key}) : super(key: key);
+
+  @override
+  State<OnBoardingConfirmScreen> createState() =>
+      _OnBoardingConfirmScreenState();
+}
+
+class _OnBoardingConfirmScreenState extends State<OnBoardingConfirmScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(''),
+      ),
+      body: Column(
+        children: [
+          const Expanded(
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 16.0),
+              child: Align(
+                alignment: Alignment.topLeft,
+                child: Text(
+                  '실천사항을 \n모두 입력했어요! \n\n열심히 계획을 세웠으니 \n이제 실천을 시작해볼까요?',
+                  style: TextStyle(
+                    fontSize: 26,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            style: ButtonStyle(
+              backgroundColor: MaterialStateProperty.resolveWith<Color>(
+                (states) {
+                  if (states.contains(MaterialState.disabled)) {
+                    return Colors.grey;
+                  }
+                  return const Color(0xff5C82FC);
+                },
+              ),
+              minimumSize: MaterialStateProperty.all(
+                const Size(double.infinity, 98),
+              ),
+              shape: MaterialStateProperty.all<BeveledRectangleBorder>(
+                BeveledRectangleBorder(
+                  borderRadius: BorderRadius.circular(0),
+                ),
+              ),
+            ),
+            child: const Text(
+              '시작하기',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 20,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
