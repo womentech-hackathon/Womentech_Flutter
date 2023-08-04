cc1500bb3ddc295e23fcb8dcbe396cbcfee92b93
yeonju <97602748+yeondooo@users.noreply.github.com>
2023-08-04 23:44:21 +0900

    fix: 생성자 추가

--- lib/presentation/components/select_index_container/select_index_container.dart
+++ lib/presentation/components/select_index_container/select_index_container.dart
@@ -3,11 +3,21 @@ import 'package:flutter/material.dart';
 class SelectIndexContainer extends StatefulWidget {
   final String text;
   final bool hasShadow;
+  final double borderRadius;
+  final Color unSelectedColor;
+  final Color unSelectedTextColor;
+  final double verticalPadding;
+  final double horizontalPadding;
 
   const SelectIndexContainer({
     Key? key,
     required this.text,
     required this.hasShadow,
+    required this.borderRadius,
+    required this.unSelectedColor,
+    required this.unSelectedTextColor,
+    required this.verticalPadding,
+    required this.horizontalPadding,
   }) : super(key: key);
 
   @override
@@ -31,11 +41,15 @@ class _SelectIndexContainerState extends State<SelectIndexContainer> {
       },
       child: Container(
         decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(12),
+          borderRadius: BorderRadius.circular(widget.borderRadius),
           border: Border.all(
-            color: const Color(0xff5C82FC),
+            color: selectedIndex == 0
+                ? const Color(0xff5C82FC)
+                : widget.unSelectedTextColor,
           ),
-          color: selectedIndex == 0 ? const Color(0xff5C82FC) : Colors.white,
+          color: selectedIndex == 0
+              ? const Color(0xff5C82FC)
+              : widget.unSelectedColor,
           boxShadow: widget.hasShadow
               ? [
                   BoxShadow(
@@ -48,17 +62,18 @@ class _SelectIndexContainerState extends State<SelectIndexContainer> {
               : null,
         ),
         child: Padding(
-          padding: const EdgeInsets.symmetric(
-            horizontal: 11.0,
-            vertical: 7.0,
+          padding: EdgeInsets.symmetric(
+            horizontal: widget.horizontalPadding,
+            vertical: widget.verticalPadding,
           ),
           child: Text(
             widget.text,
             style: TextStyle(
               fontSize: 18,
               fontWeight: FontWeight.w600,
-              color:
-                  selectedIndex == 0 ? Colors.white : const Color(0xff5C82FC),
+              color: selectedIndex == 0
+                  ? Colors.white
+                  : widget.unSelectedTextColor,
             ),
             textAlign: TextAlign.left,
           ),
