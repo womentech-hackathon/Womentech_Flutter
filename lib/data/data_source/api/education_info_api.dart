223c7035e50ff84dcdf25c3f1a2604522a03ab77
yeonju <97602748+yeondooo@users.noreply.github.com>
2023-08-11 18:45:58 +0900

    feat: education info api 구현

---
+++ lib/data/data_source/api/education_info_api.dart
@@ -0,0 +1,18 @@
+import 'dart:convert';
+import 'package:women_tech_flutter/data/dto/education_data_result_dto.dart';
+import 'package:http/http.dart' as http;
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+class EducationIntoApi {
+  final _baseUrl = dotenv.env['BASE_URL'];
+  final _key = dotenv.env['KEY'];
+
+  Future<EducationDataResultDto> getEducationData(
+      int startIndex, int endIndex) async {
+    final url = '$_baseUrl/$_key/json/tbViewProgram/$startIndex/$endIndex';
+    final http.Response response = await http.get(Uri.parse(url));
+
+    Map<String, dynamic> jsonString = jsonDecode(response.body);
+    return EducationDataResultDto.fromJson(jsonString);
+  }
+}
