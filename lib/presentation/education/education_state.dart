4dbf6720875f918708f6ac82c080871712e0cb7e
yeonju <97602748+yeondooo@users.noreply.github.com>
2023-08-12 08:58:40 +0900

    feat: education state 구현

---
+++ lib/presentation/education/education_state.dart
@@ -0,0 +1,18 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:flutter/foundation.dart';
+import 'package:women_tech_flutter/domain/education/education_info.dart';
+
+part 'education_state.freezed.dart';
+
+part 'education_state.g.dart';
+
+@freezed
+class EducationState with _$EducationState {
+  const factory EducationState({
+    @Default([]) List<EducationInfo> educationInfoList,
+    @Default(false) bool isLoading,
+  }) = _EducationState;
+
+  factory EducationState.fromJson(Map<String, Object?> json) =>
+      _$EducationStateFromJson(json);
+}
