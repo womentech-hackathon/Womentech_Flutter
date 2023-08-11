d9863e451dc720a13038b63f457246d1a9a1e532
yeonju <97602748+yeondooo@users.noreply.github.com>
2023-08-12 08:58:01 +0900

    feat: education repository impl 구현

---
+++ lib/data/repository/education_repository_impl.dart
@@ -0,0 +1,20 @@
+import 'package:women_tech_flutter/data/data_source/api/education_info_api.dart';
+import 'package:women_tech_flutter/data/mapper/education_info_mapper.dart';
+import 'package:women_tech_flutter/domain/education/education_data.dart';
+import 'package:women_tech_flutter/domain/repository/education_repository.dart';
+
+class EducationRepositoryImpl implements EducationRepository {
+  final EducationInfoApi _api;
+
+  EducationRepositoryImpl(this._api);
+
+  @override
+  Future<EducationData> getEducationData(int startIndex, int endIndex) async {
+    try {
+      final resultDto = await _api.getEducationData(startIndex, endIndex);
+      return resultDto.toEducationData();
+    } catch (e) {
+      throw Exception('education api 에러');
+    }
+  }
+}
