8b434d6bacf225fd7b948383e57d8cbdf1aa7a7a
yeonju <97602748+yeondooo@users.noreply.github.com>
2023-08-21 23:11:59 +0900

    fix: education dto null safety 적용한것으로 수정

--- lib/data/dto/education_data_result_dto.dart
+++ lib/data/dto/education_data_result_dto.dart
@@ -1,132 +1,119 @@
 class EducationDataResultDto {
   EducationDataResultDto({
-    this.tbViewProgram,
+    required this.tbViewProgram,
   });
 
-  EducationDataResultDto.fromJson(dynamic json) {
-    tbViewProgram = json['tbViewProgram'] != null
-        ? TbViewProgram.fromJson(json['tbViewProgram'])
-        : null;
+  late final TbViewProgram tbViewProgram;
+
+  EducationDataResultDto.fromJson(Map<String, dynamic> json) {
+    tbViewProgram = TbViewProgram.fromJson(json['tbViewProgram']);
   }
 
-  TbViewProgram? tbViewProgram;
-
   Map<String, dynamic> toJson() {
-    final map = <String, dynamic>{};
-    if (tbViewProgram != null) {
-      map['tbViewProgram'] = tbViewProgram?.toJson();
-    }
-    return map;
+    final _data = <String, dynamic>{};
+    _data['tbViewProgram'] = tbViewProgram.toJson();
+    return _data;
   }
 }
 
 class TbViewProgram {
   TbViewProgram({
-    this.listTotalCount,
-    this.result,
-    this.row,
+    required this.listTotalCount,
+    required this.result,
+    required this.row,
   });
 
-  TbViewProgram.fromJson(dynamic json) {
+  late final int listTotalCount;
+  late final Result result;
+  late final List<Row> row;
+
+  TbViewProgram.fromJson(Map<String, dynamic> json) {
     listTotalCount = json['list_total_count'];
-    result = json['RESULT'] != null ? Result.fromJson(json['RESULT']) : null;
-    if (json['row'] != null) {
-      row = [];
-      json['row'].forEach((v) {
-        row?.add(Row.fromJson(v));
-      });
-    }
+    result = Result.fromJson(json['RESULT']);
+    row = List.from(json['row']).map((e) => Row.fromJson(e)).toList();
   }
-
-  num? listTotalCount;
-  Result? result;
-  List<Row>? row;
 
   Map<String, dynamic> toJson() {
-    final map = <String, dynamic>{};
-    map['list_total_count'] = listTotalCount;
-    if (result != null) {
-      map['RESULT'] = result?.toJson();
-    }
-    if (row != null) {
-      map['row'] = row?.map((v) => v.toJson()).toList();
-    }
-    return map;
-  }
-}
-
-class Row {
-  Row({
-    this.idx,
-    this.subject,
-    this.startdate,
-    this.enddate,
-    this.applicationstartdate,
-    this.applicationenddate,
-    this.registpeople,
-    this.registcost,
-    this.applystate,
-    this.viewdetail,
-  });
-
-  Row.fromJson(dynamic json) {
-    idx = json['IDX'];
-    subject = json['SUBJECT'];
-    startdate = json['STARTDATE'];
-    enddate = json['ENDDATE'];
-    applicationstartdate = json['APPLICATIONSTARTDATE'];
-    applicationenddate = json['APPLICATIONENDDATE'];
-    registpeople = json['REGISTPEOPLE'];
-    registcost = json['REGISTCOST'];
-    applystate = json['APPLY_STATE'];
-    viewdetail = json['VIEWDETAIL'];
-  }
-
-  String? idx;
-  String? subject;
-  String? startdate;
-  String? enddate;
-  String? applicationstartdate;
-  String? applicationenddate;
-  String? registpeople;
-  String? registcost;
-  String? applystate;
-  String? viewdetail;
-
-  Map<String, dynamic> toJson() {
-    final map = <String, dynamic>{};
-    map['IDX'] = idx;
-    map['SUBJECT'] = subject;
-    map['STARTDATE'] = startdate;
-    map['ENDDATE'] = enddate;
-    map['APPLICATIONSTARTDATE'] = applicationstartdate;
-    map['APPLICATIONENDDATE'] = applicationenddate;
-    map['REGISTPEOPLE'] = registpeople;
-    map['REGISTCOST'] = registcost;
-    map['APPLY_STATE'] = applystate;
-    map['VIEWDETAIL'] = viewdetail;
-    return map;
+    final _data = <String, dynamic>{};
+    _data['list_total_count'] = listTotalCount;
+    _data['RESULT'] = result.toJson();
+    _data['row'] = row.map((e) => e.toJson()).toList();
+    return _data;
   }
 }
 
 class Result {
   Result({
-    this.code,
-    this.message,
+    required this.CODE,
+    required this.MESSAGE,
   });
 
-  Result.fromJson(dynamic json) {
-    code = json['CODE'];
-    message = json['MESSAGE'];
+  late final String CODE;
+  late final String MESSAGE;
+
+  Result.fromJson(Map<String, dynamic> json) {
+    CODE = json['CODE'];
+    MESSAGE = json['MESSAGE'];
   }
 
-  String? code;
-  String? message;
+  Map<String, dynamic> toJson() {
+    final _data = <String, dynamic>{};
+    _data['CODE'] = CODE;
+    _data['MESSAGE'] = MESSAGE;
+    return _data;
+  }
+}
+
+class Row {
+  Row({
+    required this.IDX,
+    required this.SUBJECT,
+    required this.STARTDATE,
+    required this.ENDDATE,
+    required this.APPLICATIONSTARTDATE,
+    required this.APPLICATIONENDDATE,
+    required this.REGISTPEOPLE,
+    required this.REGISTCOST,
+    required this.APPLYSTATE,
+    required this.VIEWDETAIL,
+  });
+
+  late final String IDX;
+  late final String SUBJECT;
+  late final String STARTDATE;
+  late final String ENDDATE;
+  late final String APPLICATIONSTARTDATE;
+  late final String APPLICATIONENDDATE;
+  late final String REGISTPEOPLE;
+  late final String REGISTCOST;
+  late final String APPLYSTATE;
+  late final String VIEWDETAIL;
+
+  Row.fromJson(Map<String, dynamic> json) {
+    IDX = json['IDX'];
+    SUBJECT = json['SUBJECT'];
+    STARTDATE = json['STARTDATE'];
+    ENDDATE = json['ENDDATE'];
+    APPLICATIONSTARTDATE = json['APPLICATIONSTARTDATE'];
+    APPLICATIONENDDATE = json['APPLICATIONENDDATE'];
+    REGISTPEOPLE = json['REGISTPEOPLE'];
+    REGISTCOST = json['REGISTCOST'];
+    APPLYSTATE = json['APPLY_STATE'];
+    VIEWDETAIL = json['VIEWDETAIL'];
+  }
 
   Map<String, dynamic> toJson() {
-    final map = <String, dynamic>{};
-    map['CODE'] = code;
-    map['MESSAGE'] = message;
-    return map;
+    final _data = <String, dynamic>{};
+    _data['IDX'] = IDX;
+    _data['SUBJECT'] = SUBJECT;
+    _data['STARTDATE'] = STARTDATE;
+    _data['ENDDATE'] = ENDDATE;
+    _data['APPLICATIONSTARTDATE'] = APPLICATIONSTARTDATE;
+    _data['APPLICATIONENDDATE'] = APPLICATIONENDDATE;
+    _data['REGISTPEOPLE'] = REGISTPEOPLE;
+    _data['REGISTCOST'] = REGISTCOST;
+    _data['APPLY_STATE'] = APPLYSTATE;
+    _data['VIEWDETAIL'] = VIEWDETAIL;
+    return _data;
   }
 }
